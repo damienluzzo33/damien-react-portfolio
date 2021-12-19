660d43a3b0bdee61a2b3702bd9944c8881ca63a8
Damien Luzzo <damienluzzo33@gmail.com>
2021-12-19 01:28:14 -0600

    add modal and button to show pop up window with project description contained

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import CodeIcon from '@mui/icons-material/Code';
 import LinkIcon from '@mui/icons-material/Link';
-import DescriptionIcon from '@mui/icons-material/Description';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import DescriptionModal from './DescriptionModal'
+
+//* potentially turn th tech stack into its own dropdown or separate modal with a stack icon as the button
+// import TableRowsIcon from '@mui/icons-material/TableRows';
 
 export default function Projects() {
+
+	const [chosenProject, setChosenProject] = React.useState("")
+
+	const [open, setOpen] = React.useState(false);
+	const handleOpen = (event) => {
+		const projectId = event.target.parentElement.id;
+		console.log(projectId)
+		setChosenProject(projectId);
+		openModal()
+	}
+
+	const openModal = () => {
+		setOpen(true);
+	}
+
+	const handleClose = () => setOpen(false);
+
+	const style = {
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: 400,
+		bgcolor: 'background.paper',
+		border: '2px solid #000',
+		boxShadow: 24,
+		p: 4,
+	};
 	return (
 		<section id="projects" className="projects">
 			<h2 className="projects-title">Projects</h2>
@@ -18,9 +51,9 @@ export default function Projects() {
 						<a href="https://the-grouper-app.herokuapp.com/" rel="noopener noreferrer" target="_blank">
 							<LinkIcon className="material" fontSize="large" />
 						</a>
-						<a href="https://the-grouper-app.herokuapp.com/" rel="noopener noreferrer" target="_blank">
+						<div id="grouperApp" onClick={handleOpen}>
 							<MoreHorizIcon className="material" fontSize="large" />
-						</a>
+						</div>
 					</div>
 					<ul>
 						<li>Javascript</li>
@@ -40,11 +73,15 @@ export default function Projects() {
 						>{' '}
 							<CodeIcon className="material" fontSize="large" />
 						</a>
+						<div id="colorPaletteMaker" onClick={handleOpen}>
+							<MoreHorizIcon className="material" fontSize="large" />
+						</div>
 					</div>
 					<ul>
 						<li>React</li>
 						<li>Node</li>
 						<li>Express</li>
+						<li>CSS</li>
 					</ul>
 				</p>
 			</figure>
@@ -59,7 +96,17 @@ export default function Projects() {
 						<a href="https://damienluzzo33.github.io/Project-1/" rel="noopener noreferrer" target="_blank">
 							<LinkIcon className="material" fontSize="large" />
 						</a>
+						<div id="dynamiteRecipes" onClick={handleOpen}>
+							<MoreHorizIcon className="material" fontSize="large" />
+						</div>
 					</div>
+					<ul>
+						<li>Javascript</li>
+						<li>Node</li>
+						<li>Spoonacular</li>
+						<li>Materialize</li>
+						<li>CSS</li>
+					</ul>
 				</p>
 			</figure>
 			<figure className="sub-project">
@@ -72,7 +119,19 @@ export default function Projects() {
 						<a href="https://git-bloggin.herokuapp.com/" rel="noopener noreferrer" target="_blank">
 							<LinkIcon className="material" fontSize="large" />
 						</a>
+						<div id="gitBloggin" onClick={handleOpen}>
+							<MoreHorizIcon className="material" fontSize="large" />
+						</div>
 					</div>
+					<ul>
+						<li>Javascript</li>
+						<li>Node</li>
+						<li>Handlebars</li>
+						<li>Express</li>
+						<li>MySQL</li>
+						<li>Sequelize</li>
+						<li>CSS</li>
+					</ul>
 				</p>
 			</figure>
 			<figure className="sub-project">
@@ -85,6 +144,9 @@ export default function Projects() {
 						<a href="https://duly-noted-application.herokuapp.com/" rel="noopener noreferrer" target="_blank">
 							<LinkIcon className="material" fontSize="large" />
 						</a>
+						<div id="dulyNoted" onClick={handleOpen}>
+							<MoreHorizIcon className="material" fontSize="large" />
+						</div>
 					</div>
 					<ul>
 						<li>Javascript</li>
@@ -93,6 +155,16 @@ export default function Projects() {
 					</ul>
 				</p>
 			</figure>
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description"
+			>
+				<Box sx={style}>
+					<DescriptionModal projectName={chosenProject} />
+				</Box>
+			</Modal>
 		</section>
 	);
 }
