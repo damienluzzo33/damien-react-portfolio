e7830f3407ced07586166ac3d0b919e820f5acc7
Damien Luzzo <damienluzzo33@gmail.com>
2021-12-28 23:59:36 -0600

    get to work on revamping the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import EmailIcon from '@mui/icons-material/Email';
+import ArticleIcon from '@mui/icons-material/Article';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 export default function Home() {
 	return (<div>
@@ -7,6 +12,72 @@ export default function Home() {
 				<h2>Damien Luzzo</h2>
 				<h3>Software Engineer</h3>
 			</article>
+		</section>
+		<section id="preview">
+			<div id="aboutPreview">
+				<div className='previewBox'>
+					<h2>About Me</h2>
+				</div>
+				<div className='previewBanner'>
+					<p>A few sentences about me... maybe from LinkedIn?</p>
+				</div>
+			</div>
+			<div id="projectsPreview">
+				<div className='previewBox'>
+					<h2>My Projects</h2>
+				</div>
+				<div className='previewBanner'>
+					<div className='projectPicture'>
+						<div id="projectPreview1">Project1</div>
+						<p>Some Information About The Project</p>
+					</div>
+					<div className='projectPicture'>
+						<div id="projectPreview2">Project2</div>
+						<p>Some Information About The Project</p>
+					</div>
+					<div className='projectPicture'>
+						<div id="projectPreview3">Project3</div>
+						<p>Some Information About The Project</p>
+					</div>
+				</div>
+			</div>
+			<div id="recPreview">
+				<div className='previewBox'>
+					<h2>Recommendations</h2>
+				</div>
+				<div className='previewBanner'>
+					<div id="rec1">
+						<div id="recImage1">Image1</div>
+						<p className='recMessage'>A nice message</p>
+					</div>
+					<div id="rec2">
+						<div id="recImage2">Image2</div>
+						<p className='recMessage'>A nice message</p>
+					</div>
+				</div>
+			</div>
+			<div id="contactPreview">
+				<div className='previewBox'>
+					<h2>Contact Me</h2>
+				</div>
+				<div className='previewBanner'>
+					<a href="mailto:damienluzzo33@gmail.com" target="_blank" rel="noreferrer">
+						<EmailIcon aria-label="email" className='contactIcons' fontSize="large"/>
+					</a>
+					<a href="https://docs.google.com/document/d/1xvewE8IoywnYsMzn7ZHutwNR-oBfAE36FZ0bYNKNujc/edit?usp=sharing" target="_blank" rel="noreferrer">
+						<ArticleIcon aria-label="email" className='contactIcons' fontSize="large"/>
+					</a>
+					<a href="https://github.com/damienluzzo33" target="_blank" rel="noreferrer">
+						<GitHubIcon aria-label="email" className='contactIcons' fontSize="large"/>
+					</a>
+					<a href="https://www.linkedin.com/in/damienluzzo/" target="_blank" rel="noreferrer">
+						<LinkedInIcon aria-label="email" className='contactIcons' fontSize="large"/>
+					</a>
+					<a href="https://twitter.com/DamienLuzzo" target="_blank" rel="noreferrer">
+						<TwitterIcon aria-label="email" className='contactIcons' fontSize="large"/>
+					</a>
+				</div>
+			</div>
 		</section>
         </div>
 	);
