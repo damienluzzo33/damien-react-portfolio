d5a25b62ed42ff75afe30a7d06fa265c0825b7bf
Damien Luzzo <damienluzzo33@gmail.com>
2021-12-31 01:51:22 -0600

    finish home page functionality and adjust some styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,41 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/Twitter';
 
 export default function Home() {
+
+	const [open, setOpen] = React.useState(
+		{
+			project1: false,
+			project2: false,
+			project3: false
+		}
+	)
+
+	const handleOpen = (event) => {
+		let project = event.target.id;
+		if (project === "projectPreview1") {
+			let openStatus = open.project1;
+			setOpen({
+				project1: !openStatus,
+				project2: false,
+				project3: false
+			})
+		} else if (project === "projectPreview2") {
+			let openStatus = open.project2;
+			setOpen({
+				project1: false,
+				project2: !openStatus,
+				project3: false
+			})
+		} else if (project === "projectPreview3") {
+			let openStatus = open.project3;
+			setOpen({
+				project1: false,
+				project2: false,
+				project3: !openStatus
+			})
+		}
+	}
+
 	return (<div>
 		<section id="home">
 			<article>
@@ -22,7 +57,7 @@ export default function Home() {
 					<p>Software engineer and full-stack web developer with bachelor’s degrees in Physics and Philosophy. A natural problem solver, an excellent communicator, and someone who thrives in high-pressure and fast-paced environments. A proven leader and team player who enjoys supporting and mentoring others, who is patient, humble, and open to feedback, and who is always eager to learn more. Equipped with all of the skills necessary to be a productive, proactive, and indispensable asset for whatever company I work for.
 					</p>
 					<p>
-					Interested in UX/UI, artificial intelligence, backend development, and creating applications that deliver convenience, accessibility, and unique experiences to users. Hobbies and prior work experience include physics, filmmaking, environmental activism, project management, teaching, and coding. Passionate about learning, artificial consciousness, climate change, and social justice.</p>
+					Interested in UX/UI, artificial intelligence, backend development, and creating applications that deliver convenience, accessibility, and unique experiences to users. Hobbies and prior work experience include physics, filmmaking, environmental activism, project management, teaching, and coding. Passionate about learning, artificial consciousness, combatting climate change, and advocating for social and economic justice.</p>
 				</div>
 			</div>
 			<div id="projectsPreview">
@@ -32,28 +67,49 @@ export default function Home() {
 				<div className='previewBanner'>
 					<div className='projectDropdown'>
 						<p>Cactus Social</p>
-						<button>v</button>
+						<button id="projectPreview1" onClick={handleOpen}>v</button>
 					</div>
-					<div className='projectPicture'>
-						<div id="projectPreview1">Project1</div>
-						<p>Some Information About The Project</p>
-					</div>
+					{ open.project1 ? (
+						<div 
+						className='projectPicture' 
+						style={{height: open.project1 ? "100%" : "0px"}}
+					>
+						<div id="cactusSocial" style={{height: open.project1 ? "300px" : "0px"}} />
+						<p style={{height: open.project1 ? "100%" : "0px"}}>An intuitive, effective, and elegant social media platform for developers that allows users to build communities, communicate with their connections, and engage in direct messaging with their colleagues and friends.</p>
+						</div>
+					) : (
+						<></>
+					)
+					}
 					<div className='projectDropdown'>
 						<p>Grouper</p>
-						<button>v</button>
+						<button id="projectPreview2" onClick={handleOpen}>v</button>
 					</div>
-					<div className='projectPicture'>
-						<div id="projectPreview2">Project2</div>
-						<p>Some Information About The Project</p>
+					{ open.project2 ? (
+					<div className='projectPicture'  style={{height: open.project2 ? "110%" : "0px"}} >
+						<div id="grouperApp" style={{height: open.project2 ? "300px" : "0px"}} />
+						<p style={{height: open.project2 ? "100%" : "0px"}}>Allows Austin-based users to create and attend events to hang out with friends and to make new ones. User’s RSVPs and create events are sustained and saved to a MySQL database.</p>
 					</div>
+					) : (
+						<></>
+					)
+					}
 					<div className='projectDropdown'>
 						<p>Git Bloggin</p>
-						<button>v</button>
+						<button id="projectPreview3" onClick={handleOpen}>v</button>
 					</div>
-					<div className='projectPicture'>
-						<div id="projectPreview3">Project3</div>
-						<p>Some Information About The Project</p>
+					{ open.project3 ? (
+					<div 
+						className='projectPicture'
+						style={{height: open.project3 ? "100%" : "0px"}}
+					>
+						<div id="gitBloggin" style={{height: open.project3 ? "300px" : "0px"}}/>
+						<p style={{height: open.project3 ? "100%" : "0px"}}>An efficient and simple to use blogging application for developers and students to exchange ideas, share experiences, and create community. This app uses MySQL to store chat histories and allows uses to start new blogs and comment on blogs by others.</p>
 					</div>
+					) : (
+						<></>
+					)
+					}
 				</div>
 			</div>
 			<div id="recPreview">
@@ -63,11 +119,13 @@ export default function Home() {
 				<div className='previewBanner'>
 					<div id="rec1">
 						<div id="recImage1">Image1</div>
-						<p className='recPreviewMessage'>A nice message</p>
+						<p className='recPreviewMessage'>"Damien stands out as one of the best students I had in my time as a TA with the Full Stack Coding Bootcamp at UT Austin. Damien not only rose to exceed every challenge but was instrumental in helping other students understand complex subjects. Additionally Damien devoted countless hours to organizing and supporting his fellow students both intellectually and emotionally. On his coding prowess alone, I would recommend Damien. That said Damien brings along many intangibles that make him indispensable."</p>
+						<p className='recSig'>- Christian Henry, Software Engineer</p>
 					</div>
 					<div id="rec2">
 						<div id="recImage2">Image2</div>
-						<p className='recPreviewMessage'>A nice message</p>
+						<p className='recPreviewMessage'>Damien is a talented full-stack developer with an incredible understanding of coding theory and the ability to translate that into great programs. He is a natural leader and teacher that is willing to take time to help others. He has a patient and humble attitude that inspires those around him. I feel incredibly lucky to have worked with him during my time at the Boot Camp. I know he'll be an asset to any company he works for and look forward to following his career in tech.</p>
+						<p className='recSig'>- Chuck Stevens, Full-Stack Web Developer</p>
 					</div>
 				</div>
 			</div>
